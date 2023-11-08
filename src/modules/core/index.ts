b790b24214edd2223e677d1e9e8973160e6f4c94
1673beta <rekishinomori@gmail.com>
2023-11-08 16:44:07 +0900

    名前が呼んでもらえるようになるはず

--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -66,7 +66,11 @@ export default class extends Module {
 		if (!msg.text.includes('って呼んで')) return false;
 		if (msg.text.startsWith('って呼んで')) return false;
 
-		const name = msg.text.match(/^(.+?)って呼んで/)![1];
+		let name = msg.text.match(/^(.+?)って呼んで/)![1];
+
+		/**メンション部分を除外する */
+
+		name = name.replace(/<@[a-zA-Z0-9_]+>/g, '');
 
 		if (name.length > 10) {
 			msg.reply(serifs.core.tooLong);
